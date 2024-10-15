4d2e57796291140be086355d8d8e030ca6ead3dc
gabilsguimaraes <gabiguimaraes2001@gmail.com>
2024-10-14 21:21:59 -0300

    arrumando raças

---
+++ src/main/kotlin/racas/BaseRaca.kt
@@ -0,0 +1,12 @@
+package org.example.racas
+
+import org.example.Raca
+
+abstract class BaseRaca : Raca {
+    override var forca: Int = 0
+    override var destreza: Int = 0
+    override var constituicao: Int = 0
+    override var inteligencia: Int = 0
+    override var sabedoria: Int = 0
+    override var carisma: Int = 0
+}
